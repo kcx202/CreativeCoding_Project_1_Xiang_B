64695b0070916554a1358e472915978be452f840
Kevin Xiang <kcx202@users.noreply.github.com>
2020-10-19 20:07:42 -0400

    Equalizer Progree
    Made some progress on Equalizer code, deciding to move forwards with it

--- Equalizer Code/js/sketch.js
+++ Equalizer Code/js/sketch.js
@@ -1,35 +1,53 @@
-
 // Kevin Xiang
 // Creating an Equalizer
 
 let barLength = [];
-let total = 20;
+let numBar = 30;
 let mid = 100;
 let i = 0;
 let wub = 0;
+let wubStatus = true;
+let sinAmp = 0;
+let sinMove = 0;
+const Y_AXIS = 1;
+let c1, c2;
+
 
 function setup() {
-  frameRate(6)
-  createCanvas(800,800);
-  for (let i = 0; i < total; i++) {
+  frameRate(20)
+  createCanvas(windowWidth, windowHeight);
+  c1 = color(204, 102, 0);
+  c2 = color(0, 102, 153);
+  for (let i = 0; i < numBar; i++) {
     barLength[i] = 100;
   }
 }
 
 function draw() {
-  i = floor(random(1,6));
-  print(i);
-  if (i === 1){
-    wub = 200;
-  }else{
-    wub = 0;
+
+  if (wubStatus === true) {
+    wub = wub + 20;
+    sinAmp = sinAmp + 15;
+  } else {
+    wub = wub - 20;
+    sinAmp = sinAmp -15;
+  }
+  print(sinAmp)
+
+  if (frameCount % 6 == 0) {
+    if (wubStatus === true) {
+      wubStatus = false
+    } else {
+      wubStatus = true;
+      sinMove = sinMove +10;
+    }
   }
 
   for (let i = 0; i < barLength.length; i++) {
-    background(127);
+    background(50);
 
-    let noise = floor(random(-40,40));
-    barLength[i] = mid + wub + noise;
+    let noise = floor(random(-20,20));
+    barLength[i] = mid + sinAmp*sin(i/3-sinMove)+ wub + noise;
 
     // Draw rectangles
     stroke(0);
@@ -37,7 +55,7 @@ function draw() {
     fill(255);
     let w = width/barLength.length;
     for (let x = 0; x < barLength.length; x++) {
-      rect(x*w,height/2-barLength[x],w,barLength[x]);
+      rect(x*w,height*3/4-barLength[x],w,barLength[x]);
     }
   }
 }
